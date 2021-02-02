e6d73ed387ff9cec7d0be91423ce3fbbd1bf5d80
Kai O'Leary <c15354951@mydit.ie>
2021-02-02 11:03:41 +0000

    Lab 2

--- Game Engines 2 Examples 2020/Assets/Scripts/BigBoid.cs
+++ Game Engines 2 Examples 2020/Assets/Scripts/BigBoid.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class BigBoid : MonoBehaviour
 {
-    
+    [Header("Boid Attributes")]
     public Vector3 velocity;
     public float speed;
     public Vector3 acceleration;
     public Vector3 force;
     public float maxSpeed = 5;
     public float maxForce = 10;
-
     public float mass = 1;
 
+    [Header("Seek")]
     public bool seekEnabled = true;
     public Transform seekTargetTransform;
     public Vector3 seekTarget;
 
+    [Header("Arrive")]
     public bool arriveEnabled = false;
     public Transform arriveTargetTransform;
     public Vector3 arriveTarget;
     public float slowingDistance = 10;
+
+    [Header("Flee")]
+    public bool fleeEnabled = false;
+    public int fleeDistance;
+    public Transform fleeTargetTransform;
+    public Vector3 fleeTarget;
+
+    [Header("Path Following")]
+    public bool followPath;
+    public Path path;
 
 
     public void OnDrawGizmos()
@@ -43,19 +54,13 @@ public class BigBoid : MonoBehaviour
 
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public Vector3 Seek(Vector3 target)
     {
         Vector3 toTarget = target - transform.position;
         Vector3 desired = toTarget.normalized * maxSpeed;
 
         return (desired - velocity);
-    } 
+    }
 
     public Vector3 Arrive(Vector3 target)
     {
@@ -66,6 +71,18 @@ public class BigBoid : MonoBehaviour
         Vector3 desired = (toTarget / dist) * clamped;
 
         return desired - velocity;
+    }
+
+    public Vector3 Flee(Vector3 target)
+    {
+        if (Vector3.Distance(transform.position, target) < fleeDistance)
+        {
+            return -Seek(target);
+        }
+        else
+        {
+            return Vector3.zero;
+        }
     }
 
     public Vector3 CalculateForce()
@@ -84,25 +101,55 @@ public class BigBoid : MonoBehaviour
         {
             if (arriveTargetTransform != null)
             {
-                arriveTarget = arriveTargetTransform.position;                
+                arriveTarget = arriveTargetTransform.position;
             }
             f += Arrive(arriveTarget);
+        }
+
+        if (fleeEnabled)
+        {
+            if (fleeTargetTransform != null)
+            {
+                fleeTarget = fleeTargetTransform.position;
+            }
+            f += Flee(fleeTarget);
         }
 
         return f;
     }
 
-    // Update is called once per frame
+    void FollowPath()
+    {
+        if(Vector3.Distance(transform.position, path.waypoints[path.currIndex]) < 5)
+        {
+            if (path.isLooped)
+            {
+                path.currIndex = (path.currIndex + 1) % path.waypoints.Count;
+            }
+            else if (path.currIndex + 1 < path.waypoints.Count)
+            {
+                path.currIndex++;
+            }
+            seekTarget = path.waypoints[path.currIndex];
+        }
+    }
+
     void Update()
     {
+        if (followPath)
+        {
+            FollowPath();
+        }
+
         force = CalculateForce();
         acceleration = force / mass;
         velocity = velocity + acceleration * Time.deltaTime;
         transform.position = transform.position + velocity * Time.deltaTime;
         speed = velocity.magnitude;
+
         if (speed > 0)
         {
             transform.forward = velocity;
-        }        
+        }
     }
 }
---
+++ Game Engines 2 Examples 2020/Assets/Scripts/Path.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public List<Vector3> waypoints;
+    public bool isLooped;
+    public int currIndex = 0;
+
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i <= waypoints.Count -1; i++)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(waypoints[i], 2);
+            Gizmos.color = Color.red;
+            if (isLooped)
+            {
+                Gizmos.DrawLine(waypoints[i], waypoints[(i+1) % waypoints.Count]);
+            }
+            else if (i + 1 < waypoints.Count)
+            {
+                Gizmos.DrawLine(waypoints[i], waypoints[i + 1]);
+            }
+        }
+    }
+
+    void Awake()
+    {
+        waypoints.Add(transform.position);
+        waypoints.Add(new Vector3(-20, 0, 0));
+        waypoints.Add(new Vector3(-20, 0, 20));
+        waypoints.Add(new Vector3(0, 0, 20));
+    }
+}
